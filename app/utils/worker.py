ab2eac3b89c98c76401e02c3f35351e1a0989c09
vipunsanjana <vipunsanjana34@gmail.com>
2025-03-19 12:32:03 +0530

    Refactor main and worker functions to enable synchronous processing, improving clarity and error handling

--- app/utils/worker.py
+++ app/utils/worker.py
@@ -1,10 +1,11 @@
-import asyncio
+from queue import Queue
+
 from app.utils.process_url import process_url
 
-async def worker(connection, queue: asyncio.Queue):
+
+async def worker(connection, queue: Queue):
     """Worker function to process URLs from the queue."""
     while not queue.empty():
-        url = await queue.get()
+        url = queue.get()
         await process_url(connection, url)
         queue.task_done()
-        
